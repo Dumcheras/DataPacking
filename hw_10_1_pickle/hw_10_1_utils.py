54ef458b491c73eae477e2fc77733829758dcdcc
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-07-05 13:05:14 +0300

    создан метод add_key_value, заполняющий основной словарь country_dict\\the add_key_value method has been created, filling in the main country_dict dictionary

--- hw_10_1_pickle/hw_10_1_utils.py
+++ hw_10_1_pickle/hw_10_1_utils.py
@@ -3,3 +3,10 @@ import pickle
 class CountryState:
     def __init__(self, country_dict: dict):
         self.country_dict = country_dict
+    def add_key_value(self, country_name, country_capital):
+        self.country_name = country_name
+        self.country_capital = country_capital
+        while len(country_dict) < 3:
+            self.country_dict[country_name] = country_capital
+            return self.country_dict
+        return 'словарь полон'
