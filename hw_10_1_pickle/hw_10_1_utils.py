6cfbbffd4cc9e77f43c1c6d9323cc193bc609280
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-07-05 13:36:56 +0300

    Добавлены отступы "\n" Заменен return на print (79)\\ Added indentation "\n" Replaced return with print (79)

--- hw_10_1_pickle/hw_10_1_utils.py
+++ hw_10_1_pickle/hw_10_1_utils.py
@@ -23,21 +23,25 @@ class CountryState:
             self.country_dict[country_name] = country_capital
             return self.country_dict
         return 'словарь полон'
+
     '''Метод удаляет пару ключ|значение по введенному при вызове ключу\\
     \\The method deletes the key|value pair based on the key entered during the call'''
+
     def del_for_key(self, country_name):
         self.country_name = country_name
         if self.country_name in country_dict:
             del self.country_dict[country_name]
             country_in_dict = ', '.join(country_dict)
-            print(f'Упс... страны {country_name} на карте нет... теперь нет.\nОстались {country_in_dict}')
+            print(f'Упс... страны {country_name} на карте нет... теперь нет.\nОстались {country_in_dict}\n')
             return self.country_dict
         else:
             country_to_del = self.country_dict.keys()
             str_country = ', '.join(country_to_del)
-            print(f'Такой страны нет. Есть вот такие: {str_country}')
+            print(f'Такой страны нет. Есть вот такие: {str_country}\n')
             return self.country_dict
+
     '''Метод ищет в словаре значение по ключу\\The method searches the dictionary for the value by key'''
+
     def get_for_name(self, country_name):
         self.country_name = country_name
         try:
@@ -49,27 +53,34 @@ class CountryState:
             print('Такой страны нет в списке целей\n')
 
     '''Метод ищет в словаре ключ по значению\\The method searches the dictionary for the key by value'''
+
     def get_for_capital(self, country_capital):
         self.country_capital = country_capital
         capital_of_country = [key for key, value in self.country_dict.items() if value == self.country_capital]
         str_capital = ', '.join(capital_of_country)
-        print(f'Это столица страны {str_capital}')
+        print(f'Это столица страны {str_capital}\n')
         return capital_of_country
+
     '''Метод редактирует значение по ключу через перезапись\\The method edits the value by key through overwriting'''
+
     def replace_capital(self, country_name):
         self.country_name = country_name
         country_name = input("В какой стране меняем столицу? ")
         del self.country_dict[country_name]
         country_capital = input('Введите название новой столицы ')
         self.country_dict[country_name] = country_capital
+
     '''Метод упаковки словаря при помощи pickle\\The method of packing a dictionary using pickle'''
+
     @staticmethod
     def pickle_to_file(pickled_dict, file_name):
         with open(file_name, 'wb') as file:
             pickle.dump(pickled_dict, file, protocol=4)
-        return "Файл упакован"
+            print("Файл упакован")
+
 
     '''Метод распаковки словаря при помощи pickle\\The method of unpacking a dictionary using pickle'''
+
     @staticmethod
     def unpickled_file(pickled_file_name):
         with open(pickled_file_name, 'rb') as file:
